db0867a5913041b6aee5035bce23d3bb9d9da6ad
ricadosribeiro <ricardo.sribeiro@outlook.com>
2023-08-29 13:09:01 -0300

    s3_bucket_with_public_access

---
+++ s3_bucket_with_public_access/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+  default_tags {
+    tags = {
+      owner       = "Ricardo Carvalhaes Ribeiro",
+      owner_email = "ricardo.sribeiro@outlook.com",
+      pourpose    = "Curso Terraform Udemy"
+    }
+  }
+}
+
